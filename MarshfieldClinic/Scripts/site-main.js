ccceeebaf2e86e61f10c76bb8d24994d6974f622
DrewMac <amccauley723@gmail.com>
2017-08-11 09:46:00 -0500

    syncing with server

--- MarshfieldClinic/Scripts/site-main.js
+++ MarshfieldClinic/Scripts/site-main.js
@@ -1,148 +1,43 @@
 
-    $('html').click(function () {
-        $('.top-level-nav > li.hasKids.active').removeClass('active');
-    });
-    $('.top-level-nav').click(function (event) {
-        event.stopPropagation();
-    });
-    $('.top-level-nav > li.hasKids > a').click(function (event) {
-        var $clickedLi = $(this);
-        $clickedLi.parent().siblings().removeClass('active');
-        if ($clickedLi.parent().hasClass('active')) {
-            $clickedLi.parent().removeClass('active');
-        } else {
-            $clickedLi.parent().addClass('active');
-            closeTypeAhead();
-        }
-    });
-    
-    
-    // Search and Type Ahead
-    $(document).on('click', '.header-test-button', function(event){
-        event.preventDefault();
-        window.location = '/Search/?query=' + $('#newSearchInput').val();
-    });
-    $(document).on('keyup', '#newSearchInput', function(event){
-        event.preventDefault();
-        if(event.keyCode === '13'){
-            window.location = '/Search/?query=' + $('#newSearchInput').val();
-        }
-        if($(this).val() !== '' && $(this).val() !== 'undefined' && $(this).val() !== null){
-            getServices($(this).val());
-            getProviders($(this).val());
-            getLocations($(this).val());
-            //getBlogPosts($(this).val());
-            $("#typeAheadResults").show();
-        }else{
-             closeTypeAhead();
-        }
-        
-    });
-    $(document).on('blur', '#newSearchInput', function(event){
-        // $("#typeAheadResults").hide();
-    });
-    $(document).on('focus', '#newSearchInput', function(event){
-        // $("#typeAheadResults").html("")
-        $(this).val("");
-    });
-    $(document).on('click', '.doc-test-button', function(event){
-        event.preventDefault();
-        window.location = '/doctors/search/?query=' + $('#docSearchInput').val();
-    });
-    $(document).on('keyup', '#docSearchInput', function(event){
-        event.preventDefault();
-        if(event.keyCode === '13'){
-            window.location = '/doctors/search/?query=' + $('#docSearchInput').val();
-        }
-    });
-    
-function getProviders(searchTerms){
-        $.getJSON('/umbraco/Surface/SearchByType/Doctors?t=' + searchTerms)
-            .done(function (data) {
-                
-                if(data.length > 0){
-                    html = '<ul>';
-                    for (var i = 0; i < data.length; i++){
+$('html').click(function () {
+    $('.top-level-nav > li.hasKids.active').removeClass('active');
+});
+$('.top-level-nav').click(function (event) {
+    event.stopPropagation();
+});
+$('.top-level-nav > li.hasKids > a').click(function (event) {
+    var $clickedLi = $(this);
+    $clickedLi.parent().siblings().removeClass('active');
+    if ($clickedLi.parent().hasClass('active')) {
+        $clickedLi.parent().removeClass('active');
+    } else {
+        $clickedLi.parent().addClass('active');
+        closeTypeAhead();
+    }
+});
 
-                         html += '<li><a href="' + data[i].NiceUrl + '">' + data[i].Name  + '</a></li>'; 
-                     
-                    }
-                    html += '</ul><div style="clear: both;"></div>';
-                    $("#typeAheadProviders > .menu-items").html(html);
-                   
-                }else{
-                     $("#typeAheadProviders > .menu-items").html("No results for this topic.");
-                }
-            });
+$('button[data-cmw="false"]').on("click", function(){
+    $("#cmw-no-good").hide();
+    $("#cmw-good").show();
+}); 
+$("button[data-cmw='true']").on("click", function(){
+    $("#cmw-no-good").show();
+    $("#cmw-good").hide();
+}); 
+function checkMobileScreen(){
+    return window.innerWidth <= 700;
 }
-function getServices(searchTerms){
-    $.getJSON('/umbraco/Surface/SearchByType/Specialties?t=' + searchTerms)
-            .done(function(data){
-                if(data.length > 0){
-                    html = '<ul>';
-                    for(var i = 0; i < data.length; i++){
-                        html += '<li><a href="' + data[i].NiceUrl + '">' + data[i].Name  + '</a></li>';
-                    }
-                    html += '</ul><div style="clear: both;"></div>';
-                    $("#typeAheadServices > .menu-items").html(html);
-                }else{
-                     $("#typeAheadServices > .menu-items").html("No results for this topic.");
-                }
-            });
-}
-    function getLocations(searchTerms){
-        $.getJSON('/umbraco/Surface/SearchByType/Locations?t=' + searchTerms)
-            .done(function(data){
-                if(data.length > 0){
-                    html = '<ul>';
-                    for(var i = 0; i < data.length; i++){
-                        html += '<li><a href="' + data[i].NiceUrl + '">' + data[i].Name  + '</a></li>';
-                    }
-                    html += '</ul><div style="clear: both;"></div>';
-                    $("#typeAheadLocations > .menu-items").html(html);
-                }else{
-                    $("#typeAheadLocations > .menu-items").html("No results for this topic.");
-                }
-            });
-    }
-    function getBlogPosts(searchTerms){
-        $.ajax({
-            url: 'http://shine365.marshfieldclinic.org/wp-json/wp/v2/posts',
-            data: {
-                filter: {
-                'per_page': 3,
-                'search': searchTerms
-                }
-            },
-            dataType: 'json',
-            type: 'GET',
-            success: function(data) {
-                console.log(data);
-                if(data.length > 0){
-                    html = '<ul>';
-                    for(var i = 0; i < 3; i++){
-                        html += '<li><a href="' + data[i].link + '">' + data[i].title.rendered  + '</a></li>';
-                    }
-                    html += '</ul><div style="clear: both;"></div>';
-                    $("#typeAheadBlog > .menu-items").html(html);
-                }else{
-                     $("#typeAheadBlog > .menu-items").html("No results for this topic.");
-                }
-            },
-            error: function() {
-                // error code
-            }
-        });
 
-        
+$('.body-margin:first').css('margin-top', $('.all-the-header').height());
+
+$(window).resize(function(){
+    if(checkMobileScreen()){
+        $('body').addClass('mobile');
+    }else{
+        $('body').removeClass('mobile');
+        $('.body-margin:first').css('margin-top', $('.all-the-header').height());
     }
-    function closeTypeAhead(){
-        $("#typeAheadServices > .menu-items").html("");
-        $("#typeAheadLocations > .menu-items").html("");
-        $("#typeAheadBlog > .menu-items").html("");
-        $("#typeAheadProviders > .menu-items").html("");
-        $("#typeAheadResults").hide();
-    }
+});
     
 
                 
